3d6cf5d1a1ec7cf9861a74b4095cb99b1d461240
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-03-13 14:15:42 +0600

    feat: add gallery sectio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 "use strict";
 
+const accessKey = "m94e_SKFZ0ug15ekNNXXznLH3IDU9QvV4KaSMAMVSxM";
+
+const randomPhotoUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=30`;
+
+const gallery = document.querySelector(".gallery");
+
+let allImages;
+
+const getImages = async function () {
+  const res = await fetch(randomPhotoUrl);
+  const data = await res.json();
+  allImages = data;
+  makeImages(allImages);
+};
+getImages();
+
+const makeImages = (data) => {
+  data.forEach((item, index) => {
+
+    let img = document.createElement("img");
+    img.src = item.urls.regular;
+    img.className = 'gallery-img';
+
+    gallery.appendChild(img);
+  });
+};
+
+
+
 
