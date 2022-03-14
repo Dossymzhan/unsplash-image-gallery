0b27c8c9abe9362a92c5b33986774302f9e3955a
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-03-14 22:00:05 +0600

    feat: add popup functionality

--- js/index.js
+++ js/index.js
@@ -7,26 +7,40 @@ const randomPhotoUrl = `https://api.unsplash.com/photos/random?client_id=${acces
 const gallery = document.querySelector(".gallery");
 
 let allImages;
+let currentImg = 0;
 
 const getImages = async function () {
   const res = await fetch(randomPhotoUrl);
   const data = await res.json();
   allImages = data;
+
   makeImages(allImages);
 };
 getImages();
 
 const makeImages = (data) => {
   data.forEach((item, index) => {
-
     let img = document.createElement("img");
     img.src = item.urls.regular;
-    img.className = 'gallery-img';
+    img.className = "gallery-img";
 
     gallery.appendChild(img);
+
+    // popup image
+    img.addEventListener("click", () => {
+      currentImg = index;
+      showPopup(item);
+    });
   });
 };
 
+const showPopup = (item) => {
+  let popup = document.querySelector(".popup");
+  const downloadBtn = document.querySelector(".download-btn");
+  const closeBtn = document.querySelector(".close-btn");
+  const image = document.querySelector(".large-img");
 
-
-
+  popup.classList.remove("hide");
+  downloadBtn.href = item.links.html;
+  image.src = item.urls.regular;
+};
